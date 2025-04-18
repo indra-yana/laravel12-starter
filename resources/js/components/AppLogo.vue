17681d45ed975511b70f364c63b67a9c7fc2c933
Indra Muliana <indra.ndra26@gmail.com>
2025-04-19 06:19:11 +0700

    update sidebar

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appVersion = import.meta.env.VITE_APP_VERSION || 'v1.0.0';
+
 </script>
 
 <template>
@@ -7,6 +11,9 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
         <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate font-semibold leading-none">Laravel Starter Kit</span>
+        <div class="flex flex-col gap-0.5 leading-none">
+            <span class="truncate font-semibold leading-none">{{ appName }}</span>
+            <span class="">{{ appVersion }}</span>
+        </div>
     </div>
 </template>
