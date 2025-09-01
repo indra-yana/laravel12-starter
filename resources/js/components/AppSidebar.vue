8ee85fbc9fe741d630ec14b649b319cc947762eb
indra.ndra26 <indra.ndra26@gmail.com>
2025-09-01 21:39:44 +0700

    improve navbar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
+import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { Link } from '@inertiajs/vue3';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem } from '@/types';
+import AppLogo from './AppLogo.vue';
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { type NavItem } from '@/types';
-import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
-import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
     {
@@ -37,7 +37,7 @@ const footerNavItems: NavItem[] = [
                 <SidebarMenuItem>
                     <SidebarMenuButton size="lg" as-child>
                         <Link :href="route('dashboard')">
-                            <AppLogo />
+                        <AppLogo />
                         </Link>
                     </SidebarMenuButton>
                 </SidebarMenuItem>
--- resources/js/components/AppSidebarHeader.vue
+++ resources/js/components/AppSidebarHeader.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { onMounted, reactive } from 'vue';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import Breadcrumbs from '@/components/Breadcrumbs.vue';
 import ThemeSwitcher from './ThemeSwitcher.vue';
@@ -7,10 +8,22 @@ import type { BreadcrumbItemType } from '@/types';
 defineProps<{
     breadcrumbs?: BreadcrumbItemType[];
 }>();
+
+const data = reactive({
+	fixed: false,
+});
+
+onMounted(() => {
+	window.addEventListener("scroll", () => {
+		let scrollPos = window.scrollY;
+        data.fixed = scrollPos > 0;
+	});
+})
+
 </script>
 
 <template>
-    <header class="flex h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/70 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
+    <header class="sticky top-0 z-10 bg-background rounded-t-xl flex h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/70 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4" :class="{'bg-white/60 dark:bg-slate-900/60 backdrop-blur-md border-slate-300/50 dark:border-slate-700/50 transition-colors duration-500': data.fixed}">
         <div class="flex justify-between items-center gap-2 w-full">
             <div class="flex items-center">
                 <SidebarTrigger class="-ml-1" />
