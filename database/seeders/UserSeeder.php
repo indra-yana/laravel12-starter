5289f2c08ffc6a3a71ff8c8034ceecbd99a714b9
indra.ndra26 <indra.ndra26@gmail.com>
2025-10-15 13:25:59 +0700

    update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,16 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use Modules\Employee\Models\Employee;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
         // Cek dan buat admin user jika belum ada
-        User::firstOrCreate(
+        $admin = User::firstOrCreate(
             ['email' => 'admin@laravel.com'],
             [
                 'name' => 'Admin User',
@@ -23,13 +22,41 @@ class UserSeeder extends Seeder
             ]
         );
 
-        // Hanya jalankan di non-production
-        if (!app()->isProduction()) {
-            // Hapus semua user dummy kecuali admin
-            User::where('email', '!=', 'admin@laravel.com')->delete();
+        $superadmin = User::firstOrCreate(
+            ['email' => 'superadmin@laravel.com'],
+            [
+                'name' => 'Super Admin',
+                'email_verified_at' => now(),
+                'password' => Hash::make('supersecret'),
+                'is_active' => true,
+            ]
+        );
 
-            // Buat 1000 user dummy
-            User::factory()->count(2000)->create();
+        // Buat employee untuk admin & superadmin jika belum ada
+        Employee::firstOrCreate(['user_id' => $admin->id], [
+            'nip' => fake()->unique()->numerify('1987##############'),
+            'nik' => fake()->unique()->numerify('3278###########'),
+        ]);
+
+        Employee::firstOrCreate(['user_id' => $superadmin->id], [
+            'nip' => fake()->unique()->numerify('1988#########'),
+            'nik' => fake()->unique()->numerify('3278###########'),
+        ]);
+
+        // Hanya jalankan dummy di non-production
+        if (!app()->isProduction()) {
+            // Hapus semua user dummy kecuali admin dan superadmin
+            User::whereNotIn('email', ['admin@laravel.com', 'superadmin@laravel.com'])->delete();
+            Employee::whereNotIn('user_id', [$admin->id, $superadmin->id])->delete();
+
+            // Buat 2000 user dummy dan employee terkait
+            User::factory()->count(100)->create()->each(function ($user) {
+                Employee::create([
+                    'user_id' => $user->id,
+                    'nip' => fake()->unique()->numerify('1989##############'),
+                    'nik' => fake()->unique()->numerify('3278###########'),
+                ]);
+            });
         }
     }
 }
